057d307ae01827f92f8ff1949ffd6c4d88c752bf
amarbayar.b <amarbayar.b@itc.gov.mn>
2023-05-10 13:07:21 +0800

    email zaswar

--- src/components/contacts/ContactForm.js
+++ src/components/contacts/ContactForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import ContactAddress from './ContactAddress'
 import emailjs from "emailjs-com";
+import { Button, message } from 'antd';
 
 function ContactForm() {
   const [mailData, setMailData] = useState({
@@ -8,26 +9,40 @@ function ContactForm() {
     email: "",
     message: "",
   });
+  const [errorS, setError] = useState("");
+  const [loadingS, setLoadingS] = useState(false);
   const onChange = (e) => { 
     setMailData({ ...mailData, [e.target.name]: e.target.value })
   };
-  const emailSend = () =>{ 
-    console.log("object", mailData);
+  const emailSend = () =>{  
+    setLoadingS(true);
+    console.log("object", mailData.client_name.length);
+    if (mailData.client_name.length === 0 || mailData.email.length === 0 || mailData.message.length === 0) {
+        setError("Бүх талбарыг бөглөнө үү!!");
+        message.error("Бүх талбарыг бөглөнө үү!!");
+        setLoadingS(false);
+    }else {
+    setError("");
+    setLoadingS(true);
     emailjs.send(
           "service_vfeb1rk", // service id service_rq0sez5
           "template_3k9z01y", // template id
           mailData,
           "lD7XQ055kOW7q4V1n", // public api lD7XQ055kOW7q4V1n 
-        ).then((res) => {
-          console.log("res: ",res);
+        ).then((res) => { 
             // setError(false);
             // clearError();
-            // setMailData({ to_name: "", from_name: "", message: "" });
+            setLoadingS(false);
+            message.success("Амжилттай илгээлээ!")
+            setMailData({ to_name: "", from_name: "", message: "" });
           },
-          (err) => {
-            console.log(err);
+          (err) => { 
+            message.error("Амжилтгүй хүсэлт")
+            setLoadingS(false);
           }
       );
+    }
+   
   }
   return (
 
@@ -57,10 +72,11 @@ function ContactForm() {
                     </div>
                     <div className="form-grp checkbox-grp">
                       <input type="checkbox" id="checkbox" />
-                      <label htmlFor="checkbox">Имэйл хаягаа бүү харуул</label>
-                    </div>
-                    <button type="button" className="btn rounded-btn" onClick={emailSend}>Илгээх</button>
+                      <label htmlFor="checkbox">Имэйл хаягаа зөв бичсэн үү?</label>
+                    </div> 
+                    {/* <button type="button" className="btn rounded-btn" onClick={emailSend}  style={errorS === 0 ? {cursor: "pointer"}: {cursor: "no-drop"}}>Илгээх</button> */}
                   </form>
+                  <Button onClick={emailSend} loading={loadingS} disabled={loadingS} size='large' type='primary' style={{padding: "0px 34px", height: "50px", width: "138px", fontSize: "20px"}} className='btn'>Илгээх</Button>
                 </div>
               </div>
 
